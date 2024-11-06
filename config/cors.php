555120d954b1124dc2d7873f09c7062f2934ed33
mtalhahussain <talhahusain456@gmail.com>
2024-11-07 00:37:21 +0500

    07-nov

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://app.demo-ontech.cc','app.demo-ontech.cc', 'localhost:3000'],
+    'allowed_origins' => ['https://app.demo-ontech.cc','app.demo-ontech.cc', 'localhost:3000', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
