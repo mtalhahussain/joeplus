7d3a03b150e9986037f863c4a1328b5dd25a4c7e
mtalhahussain <talhahusain456@gmail.com>
2024-12-18 01:28:50 +0500

    due-date/Fixed

--- database/migrations/2024_11_26_201657_alter_some_tables.php
+++ database/migrations/2024_11_26_201657_alter_some_tables.php
@@ -19,16 +19,25 @@ return new class extends Migration
 
         Schema::table('task_assignees', function (Blueprint $table) {
         
-            $table->unsignedBigInteger('sub_task_id')->nullable()->change();
+            if(!Schema::hasColumn('task_assignees', 'sub_task_id')) $table->unsignedBigInteger('sub_task_id')->nullable()->change();
         });
 
         Schema::table('tasks', function (Blueprint $table) {
         
-            $table->string('priority',99)->nullable()->after('status');
+            if(!Schema::hasColumn('tasks', 'priority')) $table->string('priority',99)->nullable()->after('status');
+            if(!Schema::hasColumn('tasks', 'priority')) $table->string('priority',99)->nullable()->after('status');
+            if (Schema::hasColumn('tasks', 'due_date')) {
+                \DB::statement('ALTER TABLE tasks CHANGE COLUMN `due_date` `due_start` DATE NULL AFTER `description`');
+            }
+            if(!Schema::hasColumn('tasks', 'due_start')) $table->date('due_start')->nullable()->after('description');
+            if(!Schema::hasColumn('tasks', 'due_end')) $table->date('due_end')->nullable()->after('due_start');
+
         });
         Schema::table('sub_tasks', function (Blueprint $table) {
         
-            $table->string('priority',99)->nullable()->after('status');
+            if(!Schema::hasColumn('sub_tasks', 'priority')) $table->string('priority',99)->nullable()->after('status');
+            if(!Schema::hasColumn('sub_tasks', 'due_start')) $table->date('due_start')->nullable()->after('description');
+            if(!Schema::hasColumn('sub_tasks', 'due_end')) $table->date('due_end')->nullable()->after('due_start');
         });
     }
 
