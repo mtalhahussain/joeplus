daaace2e2eae52906c8e29a7f53a6c86aea09bbc
mtalhahussain <talhahusain456@gmail.com>
2025-02-07 00:44:34 +0500

    kk

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
         'roles',
         'google_id',
         'pivot',
-        'notifications'
+        'unread_notifications'
     ];
     
     protected $casts = [
